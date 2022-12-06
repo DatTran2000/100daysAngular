fef56ea12d77561db245e719088c2a3187e10c93
Dattran <dat.tran@gumiviet.com>
2022-12-06 13:44:57 +0700

    hihi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToogleComponent } from './toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ToogleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
